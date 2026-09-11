f0e18313a507f31145534b1fb1c7900814cba871
akapapillio <pixelshinobu@gmail.com>
2026-09-11 06:25:52 +0000

    feat(db): ajouter un moyen pour se connecter à la 2 e base

--- src/common/database/database.provider.ts
+++ src/common/database/database.provider.ts
@@ -18,3 +18,20 @@ export const databaseProvider: Provider = {
     });
   },
 };
+
+export const databaseProvider2: Provider = {
+  provide: 'DATABASE_CONNECTION2',
+  inject: [ConfigService],
+  useFactory: async (configService: ConfigService) => {
+    return mysql.createPool({
+      host: configService.get<string>('DB_HOST'),
+      port: configService.get<number>('DB_PORT'),
+      user: configService.get<string>('DB_USER'),
+      password: configService.get<string>('DB_PASSWORD'),
+      database: configService.get<string>('DB_NAME2'),
+      waitForConnections: true,
+      connectionLimit: 10,
+      queueLimit: 0,
+    });
+  },
+};
