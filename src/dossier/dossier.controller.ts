8ad5292746d6ce2a1471ce566cf9fbe12a15e4cd
akapapillio <pixelshinobu@gmail.com>
2026-09-13 19:54:45 +0000

    feat(dossier): implement read
    update dossier.controller.ts :
    end point get : /dossiers
    
    update dossier.service.ts :
    add find all function  , and optionnal filter

---
+++ src/dossier/dossier.controller.ts
@@ -0,0 +1,34 @@
+import { Controller, Get , Post , Body, Query  } from '@nestjs/common';
+import { DossierService } from './dossier.service';
+import { CreateDossierDto } from './dto/create-dossier.dto';
+
+@Controller('dossiers')
+export class DossierController {
+  constructor(private readonly dossierService: DossierService) {}
+
+  @Get('ping')
+  async ping() {
+    return this.dossierService.pingDb();
+  }
+
+  // --- ROUTE DE TEST TEMPORAIRE ---
+  @Post('test-validation')
+  testDto(@Body() dto: CreateDossierDto) {
+    return {
+      message: 'Les données sont valides et prêtes à être insérées !',
+      recu: dto
+    };
+  }
+
+  @Get()
+  async findAll(
+    @Query('divisionId') divisionId?: string,
+    @Query('status') status?: string,
+  ) {
+    // Si divisionId est fourni dans l'URL, on le convertit en nombre
+    const parsedDivisionId = divisionId ? parseInt(divisionId, 10) : undefined;
+    
+    return this.dossierService.findAll(parsedDivisionId, status);
+}
+
+}
---
+++ src/dossier/dossier.service.ts
@@ -0,0 +1,37 @@
+import { Injectable, Inject } from '@nestjs/common';
+import * as mysql from 'mysql2/promise';
+
+@Injectable()
+export class DossierService {
+  constructor(
+    @Inject('DATABASE_CONNECTION2') private readonly db2: mysql.Pool,
+  ) {}
+
+  async pingDb() {
+    // Test simple pour vérifier que la base 2 répond
+    const [rows] = await this.db2.query('SELECT 1 as is_connected');
+    return rows;
+  }
+// ÉTAPE 3 : Lister les dossiers avec filtres optionnels
+  async findAll(divisionId?: number, status?: string) {
+    let sql = 'SELECT * FROM dossiers WHERE 1=1';
+    const params: any[] = [];
+
+    if (divisionId) {
+      sql += ' AND current_division_id = ?';
+      params.push(divisionId);
+    }
+
+    if (status) {
+      sql += ' AND status = ?';
+      params.push(status);
+    }
+
+    // On trie du plus récent au plus ancien
+    sql += ' ORDER BY created_at DESC';
+
+    const [rows] = await this.db2.query(sql, params);
+    return rows;
+  }
+
+}
